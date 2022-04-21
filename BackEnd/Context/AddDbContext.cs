1a97cf139f964a2ddbcff338f7ae09482e382877
DAIANE LINHARES FERREIRA <daialinhares26@gmail.com>
2022-04-21 02:21:18 -0300

    Configuracoes para banco de dados, Entidade AppDbContext, configuracoes em appsetting.json e startup, criacao banco de dados sql

---
+++ BackEnd/Context/AddDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Context
+{
+    public class AddDbContext : DbContext
+    {
+        public AddDbContext(DbContextOptions<AddDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ BackEnd/Migrations/20220421050858_Inicial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Hobbie = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    UrlFoto = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
